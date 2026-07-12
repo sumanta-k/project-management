88a1fea1e04685a18347d1e681cad71b5c49b812
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-07-12 20:02:08 +0530

    feat: ADD password check functionality
    added password-check functionality which will take the new password
    which user is entering and will check the hash with the stored hash in
    the db.
    
    If both the hashes match ,then user has entered the correct password
    else password is wrong.

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -67,4 +67,8 @@ userSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 export const User = mongoose.model("User", userSchema);
