0e6d305b863f1d65564e22d0c61370a2e4d5e9fa
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-07-15 16:37:36 +0530

    feat: generate mails

---
+++ src/utils/mail.js
@@ -0,0 +1,42 @@
+import Mailgen from "mailgen";
+
+const emailVerificationMailgenContent = (username, verificationUrl) => {
+  return {
+    body: {
+      name: username,
+      intro: "welcome to our app! we're exited to have you on board",
+    },
+    action: {
+      instructions: "To verify your email, please click the button",
+      button: {
+        color: "#22BC06",
+        text: "verify your email",
+        link: verificationUrl,
+      },
+    },
+    outro:
+      "Need help, or have question ? just reply to this email, we would love to help",
+  };
+};
+
+const forgotPasswordMailgenContent = (username, passwordResetUrl) => {
+  return {
+    body: {
+      name: username,
+      intro: "we got a request to reset the password for your account",
+    },
+    action: {
+      instructions:
+        "To reset your password click on the following button or link",
+      button: {
+        color: "#22BC06",
+        text: "Reset password",
+        link: passwordResetUrl,
+      },
+    },
+    outro:
+      "Need help, or have question ? just reply to this email, we would love to help",
+  };
+};
+
+export { emailVerificationMailgenContent, forgotPasswordMailgenContent };
