f598fd9465be14c0cbfd0f33975779bd919da9ce
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-07-15 11:57:27 +0530

    feat: generate REFRESH_TOKEN
    generateRefreshToken function does the same way as generateAccessToken,
    it also takes an payload, secret and object which tells about expiry.

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -85,4 +85,13 @@ userSchema.methods.generateAccessToken = function () {
   );
 };
 
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: REFRESH_TOKEN_EXPIRY },
+  );
+};
 export const User = mongoose.model("User", userSchema);
