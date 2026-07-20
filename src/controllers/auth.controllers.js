5c9b1ed229b430fd8265c36b69964f8ac9143add
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-07-21 00:29:16 +0530

    FEAT: imported emailVerificationMailgenContent from mail.js file.

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -2,7 +2,7 @@ import { User } from "../models/user.models.js";
 import { ApiResponse } from "../utils/api-response.js";
 import { ApiError } from "../utils/api-error.js";
 import { asyncHandler } from "../utils/async-handler.js";
-import { sendEmail } from "../utils/mail.js";
+import { sendEmail, emailVerificationMailgenContent } from "../utils/mail.js";
 
 const generateAccessAndRefreshTokens = async (userId) => {
   try {
