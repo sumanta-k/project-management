eda37167cc249a008c97bbc66650a37a8a432d22
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-07-21 17:00:34 +0530

    FIX: fix forgot password mailgen content structure

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -59,24 +59,24 @@ const emailVerificationMailgenContent = (username, verificationUrl) => {
     }
   };
 
-const forgotPasswordMailgenContent = (username, passwordResetUrl) => {
-  return {
-    body: {
-      name: username,
-      intro: "we got a request to reset the password for your account",
-    },
-    action: {
-      instructions:
-        "To reset your password click on the following button or link",
-      button: {
-        color: "#22BC06",
-        text: "Reset password",
-        link: passwordResetUrl,
+  const forgotPasswordMailgenContent = (username, passwordResetUrl) => {
+    return {
+      body: {
+        name: username,
+        intro: "we got a request to reset the password for your account",
+      action: {
+        instructions:
+          "To reset your password click on the following button or link",
+        button: {
+          color: "#22BC06",
+          text: "Reset password",
+          link: passwordResetUrl,
+        },
       },
-    },
-    outro:
-      "Need help, or have question ? just reply to this email, we would love to help",
-  };
+      outro:
+        "Need help, or have question ? just reply to this email, we would love to help",
+    }
+  
 };
 
 export {
