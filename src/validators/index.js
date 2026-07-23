dc475d3e9d6f6eb3bd3cd1a857041386b833ed34
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-07-23 19:05:18 +0530

    FEAT: register user step wise
    first comes, userRegisterValidator() , this is going to check whether
    email is right or not same goes for username, fullName and so on.
    Second comes, validate, which is going to store all the errors which is
    going to come via
      const errors = validationResult(req);
    It will throw errors if it finds any.
    
    Fixes: #1

---
+++ src/validators/index.js
@@ -0,0 +1,26 @@
+import { body } from "express-validator";
+
+const userRegisterValidator = () => {
+  return [
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Email is invalid"),
+    body("username")
+      .trim()
+      .isEmpty()
+      .withMessage("username is required")
+      .isLowerCase()
+      .withMessage("username must be in lowercase")
+      .isLength({
+        min: 3,
+      })
+      .withMessage("username must be at least 3 characters long"),
+    body("password").trim().notEmpty().withMessage("password is required"),
+    body("fullName").optional().trim(),
+  ];
+};
+
+export { userRegisterValidator };
