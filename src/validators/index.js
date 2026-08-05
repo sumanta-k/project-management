5adadd6d158be00663a5bcca31213de2d5f389b8
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-08-05 16:54:43 +0530

    add all the validator

--- src/validators/index.js
+++ src/validators/index.js
@@ -30,4 +30,31 @@ const userLogInValidator = () => {
   ];
 };
 
-export { userRegisterValidator, userLogInValidator };
+const userChangeCurrentPasswordValidator = () => {
+  return [
+    body("oldPassword").notEmpty().withMessage("old password is required"),
+    body("newPassword").notEmpty().withMessage("new password is required"),
+  ];
+};
+
+const userForgotPasswordValidator = () => {
+  return [
+    body("email")
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Email is invalid"),
+  ];
+};
+
+const userResetForgotPasswordValidator = () => {
+  return [body("newPassword").notEmpty().withMessage("password is required")];
+};
+
+export {
+  userRegisterValidator,
+  userLogInValidator,
+  userChangeCurrentPasswordValidator,
+  userForgotPasswordValidator,
+  userResetForgotPasswordValidator,
+};
